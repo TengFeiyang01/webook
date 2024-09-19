61e9c1135d114bb5645e74f6f4b7dc1b1e34d489
Sparrow <2196442691@qq.com>
2024-09-19 09:24:32 +0800

    :sparkles: 微信扫码登录

--- webook/ioc/sms.go
+++ webook/ioc/sms.go
@@ -1,10 +1,16 @@
 package ioc
 
 import (
+	"github.com/redis/go-redis/v9"
+	"time"
 	"webook/webook/internal/service/sms"
 	"webook/webook/internal/service/sms/memory"
+	"webook/webook/internal/service/sms/ratelimit"
+	limiter "webook/webook/pkg/ratelimit"
 )
 
-func InitSMSService() sms.Service {
-	return memory.NewService()
+func InitSMSService(cmd redis.Cmdable) sms.Service {
+	svc := ratelimit.NewRatelimitSMSService(memory.NewService(),
+		limiter.NewRedisSlidingWindowLimiter(cmd, time.Second, 100))
+	return svc
 }
